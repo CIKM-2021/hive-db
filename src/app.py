70cf5dd95db438b21465eaa26d4e543e05410f49
nguyen minh <nguyenminh180798@gmail.com>
2021-04-19 17:09:11 +0700

    update app main, runserver script

--- src/app.py
+++ src/app.py
@@ -1,26 +1,23 @@
 import falcon
 
 from falcon_cors import CORS
+from logbook import Logger
 
-import src.conf as settings
 from .middleware.permissions import APIPermission
-from .core.routes import (
-    api_routes,
-    register_routes
-)
+from .conf import settings
+from .hive_db.views.accounts import TestResourceView, AccountView
+
+
+logger = Logger(__name__)
+
 
 def create_app():
     cors = CORS(allow_origins_list=settings.ALLOWED_HOSTS)
-
-    middlewares = [
-        cors.middleware,
-        APIPermission(),
-    ]
-
-    app = falcon.API(middleware=middlewares)
-
-    register_routes(api_routes, app, prefix='api/v1')
-
+    middlewares = [cors.middleware, APIPermission()]
+    app = falcon.App(middleware=middlewares)
+    app.add_route(f"/{settings.PREFIX}/test", TestResourceView())
+    app.add_route(f"/{settings.PREFIX}/accounts", AccountView())
     return app
+
 
 app = application = create_app()
