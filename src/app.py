7f2a5808e65d545e1023282b6ca4039c463ca5ad
MinhNN <minhnn@sunshinegroup.vn>
2021-05-18 16:01:28 +0700

    remove valid spec

--- src/app.py
+++ src/app.py
@@ -15,17 +15,6 @@ from .hive_db.views.comments import CommentView
 logger = Logger(__name__)
 
 
-# class TestSchema(Schema):
-#     q = fields.Str()
-
-# class Accountchema(Schema):
-#     author = fields.Str()
-#     body = fields.Str()
-#     url = fields.Str()
-#     permlink = fields.Str()
-#     post_id = fields.Str()
-
-
 def create_app():
     cors = CORS(allow_origins_list=settings.ALLOWED_HOSTS)
     middlewares = [cors.middleware, APIPermission()]
@@ -34,22 +23,6 @@ def create_app():
     app.add_route(f"/{settings.PREFIX}/accounts", AccountView())
     app.add_route(f"/{settings.PREFIX}/blocks", BlockView())
     app.add_route(f"/{settings.PREFIX}/comments", CommentView())
-    # Create an APISpec
-    # spec = APISpec(
-    #     title='Swagger Petstore',
-    #     version='1.0.0',
-    #     openapi_version='2.0',
-    #     plugins=[
-    #         FalconPlugin(app),
-    #         MarshmallowPlugin(),
-    #     ],
-    # )
-    # # Register entities and paths
-    # spec.components.schema('Test', schema=TestSchema)
-    # # spec.components.schema('Account', schema=Accountchema)
-    # # pass created resource into `path` for APISpec
-    # spec.path(resource=test_resource)
-    # print(spec.to_dict())
     return app
 
 
