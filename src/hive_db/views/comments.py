8e5f46ac7501cc55d9ec9719c46995e6d387dff3
MinhNN <minhnn@sunshinegroup.vn>
2021-05-25 15:30:26 +0700

    update sql in comment view

--- src/hive_db/views/comments.py
+++ src/hive_db/views/comments.py
@@ -15,6 +15,11 @@ from ...conf.clients import get_credentials
 logger = Logger(__name__)
 
 class CommentView(BaseResource):
+    def __init__(self):
+        super().__init__()
+        self.dataset = 'hive_zurich'
+        self.table = 'block_data_53467816_53950539_47'
+
     def on_get(self, req, resp):
         # init client
         credentials = get_credentials()
@@ -34,9 +39,9 @@ class CommentView(BaseResource):
         search = req.get_param('search')
 
         if fields is not None:
-            columns = fields.split(',')
+            columns = fields
         else:
-            columns = ['*']
+            columns = '*'
         if authors:
             authors = authors.split(',')
         if search:
@@ -61,126 +66,112 @@ class CommentView(BaseResource):
 
         if witnesses:
             query_template = """
-                SELECT @columns, TO_JSON_STRING(blocks)
-                FROM `steemit-307308.hive.block_01` AS blocks,
+                SELECT {columns}
+                FROM `steemit-307308`.{dataset}.{table},
                     UNNEST (transactions) AS transaction_unnest,
                     UNNEST (transaction_unnest.operations) AS operations_unnest
                 WHERE operations_unnest.value.title = ""
                     AND witness IN UNNEST(@witnesses)
                 LIMIT @limit
-            """
+            """.format(columns=columns, dataset=self.dataset, table=self.table)
             job_config = bigquery.QueryJobConfig(
                 query_parameters=[
-                    bigquery.ArrayQueryParameter("columns", "STRING", columns),
-                    bigquery.ScalarQueryParameter('table', 'STRING', table),
                     bigquery.ArrayQueryParameter("witnesses", "STRING", witnesses),
                     bigquery.ScalarQueryParameter('limit', 'INT64', size),
                 ]
             )
         elif ids:
             query_template = """
-                SELECT @columns, TO_JSON_STRING(blocks)
-                FROM `steemit-307308.hive.block_01` AS blocks,
+                SELECT {columns}
+                FROM `steemit-307308`.{dataset}.{table},
                     UNNEST (transactions) AS transaction_unnest,
                     UNNEST (transaction_unnest.operations) AS operations_unnest
                 WHERE operations_unnest.value.title = ""
                     AND id IN UNNEST(@ids)
                 LIMIT @limit
-            """
+            """.format(columns=columns, dataset=self.dataset, table=self.table)
             job_config = bigquery.QueryJobConfig(
                 query_parameters=[
-                    bigquery.ArrayQueryParameter("columns", "STRING", columns),
-                    bigquery.ScalarQueryParameter('table', 'STRING', table),
                     bigquery.ArrayQueryParameter("ids", "INT64", ids),
                     bigquery.ScalarQueryParameter('limit', 'INT64', size),
                 ]
             )
         elif block_ids:
             query_template = """
-                SELECT @columns, TO_JSON_STRING(blocks)
-                FROM `steemit-307308.hive.block_01` AS blocks,
+                SELECT {columns}
+                FROM `steemit-307308`.{dataset}.{table},
                     UNNEST (transactions) AS transaction_unnest,
                     UNNEST (transaction_unnest.operations) AS operations_unnest
                 WHERE operations_unnest.value.title = ""
                     AND id IN UNNEST(@block_ids)
                 LIMIT @limit
-            """
+            """.format(columns=columns, dataset=self.dataset, table=self.table)
             job_config = bigquery.QueryJobConfig(
                 query_parameters=[
-                    bigquery.ArrayQueryParameter("columns", "STRING", columns),
-                    bigquery.ScalarQueryParameter('table', 'STRING', table),
                     bigquery.ArrayQueryParameter("block_ids", "INT64", block_ids),
                     bigquery.ScalarQueryParameter('limit', 'INT64', size),
                 ]
             )
         elif authors:
             query_template = """
-                SELECT @columns, TO_JSON_STRING(blocks)
-                FROM `steemit-307308.hive.block_01` AS blocks,
+                SELECT {columns}
+                FROM `steemit-307308`.{dataset}.{table},
                     UNNEST (transactions) AS transaction_unnest,
                     UNNEST (transaction_unnest.operations) AS operations_unnest
                 WHERE operations_unnest.value.title = ""
                     AND operations_unnest.value.author IN UNNEST(@authors)
                 LIMIT @limit
-            """
+            """.format(columns=columns, dataset=self.dataset, table=self.table)
             job_config = bigquery.QueryJobConfig(
                 query_parameters=[
-                    bigquery.ArrayQueryParameter("columns", "STRING", columns),
-                    bigquery.ScalarQueryParameter('table', 'STRING', table),
                     bigquery.ArrayQueryParameter("authors", "STRING", authors),
                     bigquery.ScalarQueryParameter('limit', 'INT64', size),
                 ]
             )
         elif permlinks:
             query_template = """
-                SELECT @columns, TO_JSON_STRING(blocks)
-                FROM `steemit-307308.hive.block_01` AS blocks,
+                SELECT {columns}
+                FROM `steemit-307308`.{dataset}.{table},
                     UNNEST (transactions) AS transaction_unnest,
                     UNNEST (transaction_unnest.operations) AS operations_unnest
                 WHERE operations_unnest.value.title = ""
                     AND operations_unnest.value.permlink IN UNNEST(@permlinks)
                 LIMIT @limit
-            """
+            """.format(columns=columns, dataset=self.dataset, table=self.table)
             job_config = bigquery.QueryJobConfig(
                 query_parameters=[
-                    bigquery.ArrayQueryParameter("columns", "STRING", columns),
-                    bigquery.ScalarQueryParameter('table', 'STRING', table),
                     bigquery.ArrayQueryParameter("permlinks", "STRING", permlinks),
                     bigquery.ScalarQueryParameter('limit', 'INT64', size),
                 ]
             )
         elif search:
             query_template = """
-                SELECT @columns, TO_JSON_STRING(blocks)
-                FROM `steemit-307308.hive.block_01` AS blocks,
+                SELECT {columns}
+                FROM `steemit-307308`.{dataset}.{table},
                     UNNEST (transactions) AS transaction_unnest,
                     UNNEST (transaction_unnest.operations) AS operations_unnest
                 WHERE operations_unnest.value.title = ""
                     AND REGEXP_CONTAINS(operations_unnest.value.body, @words)
                 LIMIT @limit
-            """
+            """.format(columns=columns, dataset=self.dataset, table=self.table)
             job_config = bigquery.QueryJobConfig(
                 query_parameters=[
-                    bigquery.ArrayQueryParameter("columns", "STRING", columns),
-                    bigquery.ScalarQueryParameter('table', 'STRING', table),
                     bigquery.ScalarQueryParameter('words', 'STRING', search),
                     bigquery.ScalarQueryParameter('limit', 'INT64', size),
                 ]
             )
         elif after or before:
             query_template = """
-                SELECT @columns, TO_JSON_STRING(blocks)
-                FROM `steemit-307308.hive.block_01` AS blocks,
+                SELECT {columns}
+                FROM `steemit-307308`.{dataset}.{table},
                     UNNEST (transactions) AS transaction_unnest,
                     UNNEST (transaction_unnest.operations) AS operations_unnest
                 WHERE operations_unnest.value.title = ""
                     AND timestamp >= @after AND timestamp <= @before
                 LIMIT @limit
-            """
+            """.format(columns=columns, dataset=self.dataset, table=self.table)
             job_config = bigquery.QueryJobConfig(
                 query_parameters=[
-                    bigquery.ArrayQueryParameter("columns", "STRING", columns),
-                    bigquery.ScalarQueryParameter('table', 'STRING', table),
                     bigquery.ScalarQueryParameter("after", "TIMESTAMP", after),
                     bigquery.ScalarQueryParameter("before", "TIMESTAMP", before),
                     bigquery.ScalarQueryParameter('limit', 'INT64', size),
@@ -188,17 +179,15 @@ class CommentView(BaseResource):
             )
         else:
             query_template = """
-                SELECT @columns, TO_JSON_STRING(blocks)
-                FROM `steemit-307308.hive.block_01` AS blocks,
+                SELECT {columns}
+                FROM `steemit-307308`.{dataset}.{table},
                     UNNEST (transactions) AS transaction_unnest,
                     UNNEST (transaction_unnest.operations) AS operations_unnest
                 WHERE operations_unnest.value.title = ""
                 LIMIT @limit
-            """
+            """.format(columns=columns, dataset=self.dataset, table=self.table)
             job_config = bigquery.QueryJobConfig(
                 query_parameters=[
-                    bigquery.ArrayQueryParameter("columns", "STRING", columns),
-                    bigquery.ScalarQueryParameter('table', 'STRING', table),
                     bigquery.ScalarQueryParameter('limit', 'INT64', size),
                 ]
             )
@@ -214,7 +203,7 @@ class CommentView(BaseResource):
         print('authors {}'.format(authors))
 
         query_job = client.query(query_template, job_config=job_config)
-        print(query_job.to_dataframe())
-        df_results = query_job.to_dataframe()['f1_']
+        query_job.result()
+        df_results = query_job.to_dataframe()
         json_results = ujson.loads(df_results.to_json(orient='records'))
         self.ok(resp, json_results)
